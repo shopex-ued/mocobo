1e0b4e53ab631048d4d4cbdd464ff8ea72d97109
Tyler Chao <cz848@qq.com>
2016-08-20 18:11:44 +0800

    formatted code

--- grunt/config/shell.js
+++ grunt/config/shell.js
@@ -1,12 +1,12 @@
 // https://github.com/sindresorhus/grunt-shell
 module.exports = {
-	dir: {
-		options: {
-			stderr: false,
-			failOnError: false
-		},
-		command: ['mkdir <%= paths.dist %>', 'mkdir <%= paths.dist %>fonts'].join('&&')
-	},
+    dir: {
+        options: {
+            stderr: false,
+            failOnError: false
+        },
+        command: ['mkdir <%= paths.dist %>', 'mkdir <%= paths.dist %>fonts'].join('&&')
+    },
     font: {
         options: {},
         command: 'python <%= paths.builder %>generate.py'
